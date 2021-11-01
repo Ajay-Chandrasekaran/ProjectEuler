7006e330c5480b1e6516b7b7c365d9575755d37f
Ajay Chandrasekaran <ajaycs207@gmail.com>
2021-11-01 22:45:47 +0530

    C++

--- problem1.cpp
+++ problem1.cpp
@@ -1,16 +1,4 @@
-#include <stdio.h>
-
-long sum_upto(long limit);
-long sum_of_multiples(long limit);
-
-int main() {
-    long limit;
-    
-    scanf("%ld", &limit);
-    printf("%ld", sum_of_multiples(limit));
-
-    return 0;
-}
+#include <iostream>
 
 long sum_upto(long n) {
     return (n * (n + 1)) / 2;
@@ -20,4 +8,13 @@ long sum_of_multiples(long limit) {
     long sum = 0;
     sum = 3*sum_upto((limit-1)/3) + 5*sum_upto((limit-1)/5) - 15*sum_upto((limit-1)/15);
     return sum;
+}
+
+int main() {
+    long limit = 0;
+
+    std::cin >> limit;
+    std::cout << sum_of_multiples(limit);
+
+    return 0;
 }
